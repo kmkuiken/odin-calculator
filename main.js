fabc0455cde41b16b0ad653e348b56b069dce291
kmkuiken <kmkuiken@gmail.com>
2023-08-10 15:31:16 -0400

    Add JS references to each button
    Created references to each button on the calculator along with click
    event listeners for each

---
+++ main.js
@@ -0,0 +1,106 @@
+// ref to each button
+const allclear = document.querySelector("#allclear");
+const clear = document.querySelector("#clear");
+const signChange = document.querySelector("#sign-change");
+const divide = document.querySelector("#divide");
+const nine = document.querySelector("#9");
+const eight = document.querySelector("#8");
+const seven = document.querySelector("#7");
+const six = document.querySelector("#6");
+const five = document.querySelector("#5");
+const four = document.querySelector("#4");
+const three = document.querySelector("#3");
+const two = document.querySelector("#2");
+const one = document.querySelector("#1");
+const zero = document.querySelector("#0");
+const add = document.querySelector("#add");
+const subtract = document.querySelector("#subtract");
+const multiply = document.querySelector("#multiply");
+const decimal = document.querySelector("#devimal");
+const equals = document.querySelector("#equals");
+
+//total to store final result while history stores each individual action
+let total = 0;
+let userValue = 0;
+let history = ["0"];
+
+// divide, add, subtract, or multiply
+function addEval(eval) {
+  
+}
+
+allclear.addEventListener("click", () => {
+
+});
+
+clear.addEventListener("click", () => {
+  
+});
+
+signChange.addEventListener("click", () => {
+  
+});
+
+divide.addEventListener("click", () => {
+  
+});
+
+nine.addEventListener("click", () => {
+  
+});
+
+eight.addEventListener("click", () => {
+  
+});
+
+seven.addEventListener("click", () => {
+  
+});
+
+six.addEventListener("click", () => {
+  
+});
+
+five.addEventListener("click", () => {
+  
+});
+
+four.addEventListener("click", () => {
+  
+});
+
+three.addEventListener("click", () => {
+  
+});
+
+twh.addEventListener("click", () => {
+  
+});
+
+one.addEventListener("click", () => {
+  
+});
+
+zero.addEventListener("click", () => {
+  
+});
+
+add.addEventListener("click", () => {
+  
+});
+
+subtract.addEventListener("click", () => {
+  
+});
+
+multiply.addEventListener("click", () => {
+  
+});
+
+decimal.addEventListener("click", () => {
+  
+});
+
+equals.addEventListener("click", () => {
+  
+});
