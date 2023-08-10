c38d99abf96769b92b15501f177a4f1150807fd1
kmkuiken <kmkuiken@gmail.com>
2023-08-10 17:45:16 -0400

    Number buttons functionality
    Clikcing on each number button enters that value into the main section
    of the calculator.

--- main.js
+++ main.js
@@ -3,39 +3,59 @@ const allclear = document.querySelector("#allclear");
 const clear = document.querySelector("#clear");
 const signChange = document.querySelector("#sign-change");
 const divide = document.querySelector("#divide");
-const nine = document.querySelector("#9");
-const eight = document.querySelector("#8");
-const seven = document.querySelector("#7");
-const six = document.querySelector("#6");
-const five = document.querySelector("#5");
-const four = document.querySelector("#4");
-const three = document.querySelector("#3");
-const two = document.querySelector("#2");
-const one = document.querySelector("#1");
-const zero = document.querySelector("#0");
+const nine = document.querySelector("#nine");
+const eight = document.querySelector("#eight");
+const seven = document.querySelector("#seven");
+const six = document.querySelector("#six");
+const five = document.querySelector("#five");
+const four = document.querySelector("#four");
+const three = document.querySelector("#three");
+const two = document.querySelector("#two");
+const one = document.querySelector("#one");
+const zero = document.querySelector("#zero");
 const add = document.querySelector("#add");
 const subtract = document.querySelector("#subtract");
 const multiply = document.querySelector("#multiply");
-const decimal = document.querySelector("#devimal");
+const decimal = document.querySelector("#decimal");
 const equals = document.querySelector("#equals");
+const userOutput = document.querySelector("#user-value");
 
 //total to store final result while history stores each individual action
 let total = 0;
-let userValue = 0;
-let history = [0];
+let userValue = [0];
+let history = [];
 
 // divide, add, subtract, or multiply
 function addEval(eval) {
   // if user enters eval sign twice in a row the subsequent entries should not do anything
   if (typeof history[history.length - 1] === "number") {
     history.push(eval);
-  }
+    let userValueNum = Number(userValue.join(""));
+    switch (eval) {
+      case "+":
+        total = total + userValueNum;
 
-  if (history[history.length - 1] == ".") {
-    // remove decimal since #.000 is same as #
-    history.pop();
-    history.push(eval);
+      case "-":
+        total = total - userValueNum;
+
+      case "x":
+        total = total * userValueNum;
+
+      case "/":
+        total = total / userValueNum;
+
+      case "=":
+        history = [];
+    }
+    
+    userValue = [total];
+    userOutput.textContent = Number(userValue.join(""));
   }
+}
+
+function numEntry(num) {
+  userValue.push(num);
+  userOutput.textContent = Number(userValue.join(""));
 }
 
 allclear.addEventListener("click", () => {
@@ -51,51 +71,51 @@ clear.addEventListener("click", () => {
 });
 
 signChange.addEventListener("click", () => {
-  
+  userValue = -userValue;
 });
 
 divide.addEventListener("click", () => {
-  addEval("÷");
+  addEval("/");
 });
 
 nine.addEventListener("click", () => {
-  
+  numEntry("9");
 });
 
 eight.addEventListener("click", () => {
-  
+  numEntry("8");
 });
 
 seven.addEventListener("click", () => {
-  
+  numEntry("7");
 });
 
 six.addEventListener("click", () => {
-  
+  numEntry("6");
 });
 
 five.addEventListener("click", () => {
-  
+  numEntry("5");
 });
 
 four.addEventListener("click", () => {
-  
+  numEntry("4");
 });
 
 three.addEventListener("click", () => {
-  
+  numEntry("3");
 });
 
-twh.addEventListener("click", () => {
-  
+two.addEventListener("click", () => {
+  numEntry("2");
 });
 
 one.addEventListener("click", () => {
-  
+  numEntry("1");
 });
 
 zero.addEventListener("click", () => {
-  
+  numEntry("0");
 });
 
 add.addEventListener("click", () => {
@@ -111,9 +131,10 @@ multiply.addEventListener("click", () => {
 });
 
 decimal.addEventListener("click", () => {
-  
+  addEval(".");
 });
 
 equals.addEventListener("click", () => {
-  
+  addEval("=");
 });
+
